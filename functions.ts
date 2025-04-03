920b5a1c773f096b7f8d247230463903af87ba10
Praveen <praveen.k@xnara.com>
2025-04-03 23:40:33 +0530

    updated functions

--- functions.ts
+++ functions.ts
@@ -12,18 +12,80 @@
 
 
 // 1. Named function - Function Declaration
-function add(a, b) :string {
+// Syntax:
+/*
+
+function functionName(param1:datatype, param2: datatype) : return datatype {
+    // codes
+}
+
+const result = functionName(param1, param2)
+
+
+*/
+
+function add(a:string, b:string) :string {
     return a + b;
 }
 
+const addResult = add("Hello", "10")
+console.log(addResult); // Output: HelloTS
+
+
+function add2(a, b) {
+    return a + b;
+}
+
+const add2Result = add2(10, 45)
+console.log(add2Result); // Output: HelloTS
+
+
+
 // 2. Anonymous function (unnamed function) - Function expression
     // a. Anonymous function
-let multiply = function(a, b) {
-    return a * b;
+    // Syntax:
+    /*
+    const variable = function(parameters) : returntype{
+            // code
+        }
+
+    variable(param)
+
+    */
+let multiply  = function(a:number, b:number) {
+    return(a*b)
 };
 
-    // b. Arrow function
-let divide = (a, b) => a/b
 
-console.log(divide(10, 5))
+console.log(multiply(10,300))
+
+    // b. Arrow function
+    // Syntax:
+
+    /*
+    const variable = (parameters): returntype => {
+        // code
+    }
+    variable(param)
+    */
+    let divide = (a, b) : number => a/b
+    
+    console.log(divide(10, 5))
+
+    let divide1 = a => {
+        console.log(a**2)
+    }
+
+    this
+    
+    divide1(10)
+
+
+console.log(add1(10,30))
+
+function add1(a, b) {
+    return a + b;
+}
+
+
 
