3619a6212b50794a32a1d80bc1f1ded59c3bfeb9
Praveen <praveen.k@xnara.com>
2025-04-04 21:59:25 +0530

    functions update

--- functions.ts
+++ functions.ts
@@ -5,9 +5,9 @@
     // a. Anonymous function
     // b. Arrow function (=>)
 
-// 3. Constructor function
+// 3. Function Constructor
 // 4. IIFE (Immediately Invoked Function Expression)
-// 5. Factory function
+// 5. Factory function - Object
 
 
 
@@ -72,11 +72,10 @@ console.log(multiply(10,300))
     
     console.log(divide(10, 5))
 
-    let divide1 = a => {
+    let divide1 =  (a:number): void=> {
         console.log(a**2)
     }
 
-    this
     
     divide1(10)
 
@@ -86,6 +85,86 @@ console.log(add1(10,30))
 function add1(a, b) {
     return a + b;
 }
+
+
+// 3. Function Constructor
+// Syntax:
+// const obj = new Function('arg1', 'arg2', 'return arg1+arg2')
+
+const obj = new Function('a', 'b', 'return a+b')
+const result = obj(5,4)
+console.log(result);
+
+// 4. IIFE (Immediately Invoked Function Expression)
+
+// Syntax:
+/*
+(function(parameters){
+    // code
+})
+();
+
+*/
+
+(function(){
+    console.log("Application is running successfully")
+})();
+
+(function(name){
+    console.log(`${name} Server is running successfully`)
+})("AWS");
+
+
+
+// Optional parameter and Default parameter
+
+function add3(a?,b?,c?){
+    console.log(a+b) // undefined + undefined
+    console.log(c); // undefined
+}
+
+add3(10,40,30)
+add3(10,50)
+add3()
+
+
+
+function add4(name: string, age?: number){
+    if(age !==undefined){
+        return ("Name is :" +name+ " age is: " +age)
+    }
+        return ("Name is :"+ name)
+
+}
+
+console.log(add4("John", 30))
+console.log(add4("John"))
+
+
+function add5(name: string, age: number = 20 ){
+   console.log("Name is : " + name + " age is : " + age);
+}
+add5("Rahul")
+
+
+// Function Overloading
+function hello(name: string) : string;
+function hello(name: string, age: number) : string;
+
+function hello(name: string, age?: number){
+    if(age !==undefined){
+        return ("Name is :" +name+ " age is: " +age)
+    }
+        return ("Name is :"+ name)
+}
+
+console.log(hello("Rahul"));
+console.log(hello("Rahul", 10));
+
+
+
+
+
 
 
 
